b4b78a32645aa43c3a0119587243f6e2579ba8ee
jack <jackabnana@gmail.com>
2022-07-17 17:52:02 +0545

    first commit

--- core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import dashboard
-
-app_name = "core"
-
-urlpatterns = [
-    path('dashboard/', dashboard, name="dashboard"),
-]
--- orders/validators.py
+++
@@ -1,7 +0,0 @@
-from django.core.exceptions import ValidationError
-
-def validate_number(value):
-    try:
-        num = int(value)
-    except Exception:
-        raise ValidationError(f"{value} is not a valid phone number")
